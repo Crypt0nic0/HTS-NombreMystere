633f6d34ef093ad5d928895f65c111608fd519c2
Crypt0nic0 <n.greneche45@gmail.com>
2023-04-11 14:35:10 +0200

    project updated with while statement

--- Program.cs
+++ Program.cs
@@ -4,20 +4,26 @@ int nbChoisi = 0;
 Console.WriteLine("====== BIENVENU DANS LE NOMBRE MYSTERE ======");
 Console.WriteLine("Saisir votre pseudo : ");
 string pseudo = Console.ReadLine();
-Console.WriteLine($"{pseudo}, devinez le nombre mystere : ");
-nbChoisi = int.Parse(Console.ReadLine());
-if (nbChoisi == nbMyst)
+bool trouv = false;
+
+while (!trouv)
 {
-    Console.WriteLine($"Félicitations {pseudo}, vous avez trouvé !");
-}
-else
-{
-    if (nbChoisi > nbMyst)
+    Console.WriteLine($"{pseudo}, devinez le nombre mystere : ");
+    nbChoisi = int.Parse(Console.ReadLine());
+    if (nbChoisi == nbMyst)
     {
-        Console.WriteLine("Dommage, le nombre mystère est plus petit !");
+        trouv = true;
+        Console.WriteLine($"Félicitations {pseudo}, vous avez trouvé !");
     }
     else
     {
-        Console.WriteLine("Dommage, le nombre mystère est plus grand !");
+        if (nbChoisi > nbMyst)
+        {
+            Console.WriteLine("Dommage, le nombre mystère est plus petit !");
+        }
+        else
+        {
+            Console.WriteLine("Dommage, le nombre mystère est plus grand !");
+        }
     }
 }
