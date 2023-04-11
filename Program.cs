dfb2df275e33cc23c34b191576a940bd2b5d2776
Crypt0nic0 <n.greneche45@gmail.com>
2023-04-11 14:05:45 +0200

    Exercice finished

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿const int nbMyst = 38;
+int nbChoisi = 0;
+
+Console.WriteLine("====== BIENVENU DANS LE NOMBRE MYSTERE ======");
+Console.WriteLine("Saisir votre pseudo : ");
+string pseudo = Console.ReadLine();
+Console.WriteLine($"{pseudo}, devinez le nombre mystere : ");
+nbChoisi = int.Parse(Console.ReadLine());
+if (nbChoisi == nbMyst)
+{
+    Console.WriteLine($"Félicitations {pseudo}, vous avez trouvé !");
+}
+else
+{
+    Console.WriteLine("Dommage, c'est perdu !");
+}
